4c634815389e7897ae7ab678b502d7e6009e76a4
Timur Zhemaldinov <tim17d92@gmail.com>
2023-12-12 22:15:52 +0300

    Modify CLI with options and parameters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,10 +2,21 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0-SNAPSHOT",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    String filePath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    String filePath2;
 
     @Override
     public void run() {
@@ -13,7 +24,7 @@ public class App implements Runnable {
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
+        var exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 }
